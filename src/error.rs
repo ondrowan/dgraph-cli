2749a61570af9e95f446ef54cac621dfde9148b9
Ondrej Slinták <ondrowan@gmail.com>
2019-06-29 16:47:42 +0200

    Add different error message for server unavailability

--- src/error.rs
+++ src/error.rs
@@ -1,9 +1,14 @@
-use grpcio::Error;
+use grpcio::{Error, RpcStatusCode};
 
 pub fn parse(failure_err: failure::Error) {
     if let Ok(err) = failure_err.downcast::<Error>() {
-        if let Error::RpcFailure(f) = err {
-            eprintln!("RPC error: {}", f.details.unwrap());
+        if let Error::RpcFailure(rpc_failure) = err {
+            match rpc_failure.status {
+                RpcStatusCode::Unavailable => {
+                    eprintln!("Server is unavailable: {}", rpc_failure.details.unwrap())
+                }
+                _ => eprintln!("RPC error: {}", rpc_failure.details.unwrap()),
+            }
         }
     }
 }
